f58391f51afca131ac38042730200cb7bc37804e
Banga <prateekbanga12@gmail.com>
2018-12-04 19:02:36 +0530

    created rooms for the backend

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const app = express();
 const http = require('http').Server(app);
 const path = require("path");
 const io = require("socket.io")(http);
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 
+
+let totalUsers=0;
+let userID=0;
+let noOfRooms=Math.floor(totalUsers/2);
+const users = [
+
+
+];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
@@ -15,11 +28,22 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build/index.html');
 });
 io.on("connection", socket => {
-  console.log("a user is connected");
+  console.log("no of users are "+ ++totalUsers);
+  noOfRooms=Math.floor(totalUsers/2);
+  const temp={userID:socket.id,value:socket,roomNo:getRandomIntInclusive(0,noOfRooms)};
+  users.push(temp);
+  console.log(temp.roomNo);
+      socket.join(temp.roomNo);
   socket.on("chatMessage", msg => {
-    socket.broadcast.emit("chatMessage", msg);
+    socket.to(temp.roomNo).broadcast.emit("chatMessage", msg);
   });
-  socket.on("disconnect", socket => { console.log("user is disconnected."); });
+  socket.on("disconnect", () => {
+    let index=users.findIndex(x => x.userID===socket.id);
+     console.log("no of users are "+ --totalUsers);
+       noOfRooms=Math.floor(totalUsers/2);
+      users.splice(index,1);
+
+   });
 });
 
 
