e26cc10f852b0718df2d214c304339ad6f55fb58
Banga <prateekbanga12@gmail.com>
2018-12-04 19:03:28 +0530

    removed production from gitignore

---
+++ build/precache-manifest.cd397345189eec9a25bc81ac298f9ef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c297bf8f4a431c54996e",
+    "url": "/static/js/main.c297bf8f.chunk.js"
+  },
+  {
+    "revision": "4b0e1b2009f71150a69c",
+    "url": "/static/js/1.4b0e1b20.chunk.js"
+  },
+  {
+    "revision": "c297bf8f4a431c54996e",
+    "url": "/static/css/main.11f8185e.chunk.css"
+  },
+  {
+    "revision": "55d1a42c9790cca226fa12701a7f5ed6",
+    "url": "/index.html"
+  }
+];
