3395748618a5d434fb698a48a779a760836c8c73
Banga <prateekbanga12@gmail.com>
2018-12-05 20:35:05 +0530

    still have to fix the room thing

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const roomExist= (propertyName, inputArray) => {
 }
 
 
-
+let counter=0;
 let totalUsers=0;
 let userID=0;
 let noOfRooms=Math.floor(totalUsers/2);
@@ -39,11 +39,19 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
 const roomFullOrNot = (roomNos,users) => {
-  let counter=0;
   if(roomExist(roomNos,users)){
-      counter++;
-      //this is the thing if you can see it. 
+    console.log("room exist already");
+      const i=counter++;
+      console.log(i);
+        if(counter%2!==0){
+          const j=counter++;
+          console.log(j);
+          return true;
+        }
+        return false;
+      //this is the thing if you can see it.
   }    else{
       return false; }
 }
@@ -55,18 +63,21 @@ app.get('/', function (req, res) {
 io.on("connection", socket => {
   console.log("no of users are "+ ++totalUsers);
   noOfRooms=Math.floor(totalUsers/2);
+
   for(let done=0;done<21;done++){
-  const temp={userID:socket.id,value:socket,roomNo:getRandomIntInclusive(0,10)};
+  const temp={userID:socket.id,value:socket,roomNo:getRandomIntInclusive(0,noOfRooms)};
   users.push(temp);
+  console.log("user chooses "+temp.roomNo);
     if(roomFullOrNot("roomNo",users)===false){
-      console.log(temp.roomNo);
+      console.log("user joins "+temp.roomNo);
       socket.join(temp.roomNo);
       done=22;}
       else{
-        users.pop();}
-      }
-      if(done===21){
-        socket.disconnect();
+        users.pop();
+        console.log("can't enter "+temp.roomNo);
+        if(done===21){
+          socket.disconnect();
+        }}
       }
   socket.on("chatMessage", msg => {
     let index=users.findIndex(x => x.userID===socket.id);
