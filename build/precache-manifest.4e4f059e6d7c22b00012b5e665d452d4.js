de0d2191f968e1887ea8e4e589de818f5ffbe665
Banga <prateekbanga12@gmail.com>
2018-12-06 05:26:21 +0530

    added build folder in the master directory also

---
+++ build/precache-manifest.4e4f059e6d7c22b00012b5e665d452d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ec62067bec2342e0ccc7",
+    "url": "/static/js/main.ec62067b.chunk.js"
+  },
+  {
+    "revision": "4b0e1b2009f71150a69c",
+    "url": "/static/js/1.4b0e1b20.chunk.js"
+  },
+  {
+    "revision": "ec62067bec2342e0ccc7",
+    "url": "/static/css/main.74d0a76c.chunk.css"
+  },
+  {
+    "revision": "686c783a21d50d76195f7033de9006b0",
+    "url": "/index.html"
+  }
+];
