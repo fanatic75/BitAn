08b2996644b3b6f6126a06a1816fe7cf25912bf5
Banga <prateekbanga12@gmail.com>
2018-12-05 18:55:17 +0530

    have to fix so that rooms can take 2 people in instead of 1

--- server.js
+++ server.js
@@ -5,23 +5,48 @@ const path = require("path");
 const io = require("socket.io")(http);
 app.use(express.static(path.join(__dirname, 'build')));
 
+const roomExist= (propertyName, inputArray) => {
+  let seenDuplicate = false,
+      testObject = {};
+
+  inputArray.map(function(item) {
+    let itemPropertyName = item[propertyName];
+    if (itemPropertyName in testObject) {
+      testObject[itemPropertyName].duplicate = true;
+      item.duplicate = true;
+      seenDuplicate = true;
+    }
+    else {
+      testObject[itemPropertyName] = item;
+      delete item.duplicate;
+    }
+  });
+
+  return seenDuplicate;
+}
+
 
 
 let totalUsers=0;
 let userID=0;
 let noOfRooms=Math.floor(totalUsers/2);
-const users = [
+const users = [];
 
 
-];
-
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
+const roomFullOrNot = (roomNos,users) => {
+  let counter=0;
+  if(roomExist(roomNos,users)){
+      counter++;
+      //this is the thing if you can see it. 
+  }    else{
+      return false; }
+}
 
 
 app.get('/', function (req, res) {
@@ -30,12 +55,22 @@ app.get('/', function (req, res) {
 io.on("connection", socket => {
   console.log("no of users are "+ ++totalUsers);
   noOfRooms=Math.floor(totalUsers/2);
-  const temp={userID:socket.id,value:socket,roomNo:getRandomIntInclusive(0,noOfRooms)};
+  for(let done=0;done<21;done++){
+  const temp={userID:socket.id,value:socket,roomNo:getRandomIntInclusive(0,10)};
   users.push(temp);
-  console.log(temp.roomNo);
+    if(roomFullOrNot("roomNo",users)===false){
+      console.log(temp.roomNo);
       socket.join(temp.roomNo);
+      done=22;}
+      else{
+        users.pop();}
+      }
+      if(done===21){
+        socket.disconnect();
+      }
   socket.on("chatMessage", msg => {
-    socket.to(temp.roomNo).broadcast.emit("chatMessage", msg);
+    let index=users.findIndex(x => x.userID===socket.id);
+    socket.to(users[index].roomNo).broadcast.emit("chatMessage", msg);
   });
   socket.on("disconnect", () => {
     let index=users.findIndex(x => x.userID===socket.id);
